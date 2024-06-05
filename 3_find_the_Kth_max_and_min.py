36e3ce628433f24baf60e14614d326d7baa25934
AniGarai10 <anigarai10@gmail.com>
2024-06-05 19:38:38 +0530

    added bubble sort function

--- 3_find_the_Kth_max_and_min.py
+++ 3_find_the_Kth_max_and_min.py
@@ -10,7 +10,26 @@ def kthLargest(arr, k):
     return arr[-k]
 
 
+def sort_array(arr):
+    # sort using Bubble sort
+    for i in range(1, len(arr)):
+        for j in range(len(arr)- i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+
+def kthSmallest_s(arr, k):
+    return sort_array(arr)[k - 1]
+
+def kthLargest_s(arr, k):
+    return sort_array(arr)[-k]
+
+
 arr = [12, 3, 5, 7, 19]
 
 print(kthSmallest(arr, 2))
 print(kthLargest(arr, 2))
+
+print(kthLargest_s(arr, k=3))
+print(kthSmallest_s(arr, k=3))
